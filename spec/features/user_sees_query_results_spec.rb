d271298be5c82908e3f8f559006298980f975cc8
Richard Davis <rdavis.bacs@gmail.com>
2013-10-12 13:44:04 -0400

    removed test folder, started bdd tests

---
+++ spec/features/user_sees_query_results_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature 'user can see the results of a query on the TownHealthRecord database', %q{
+  As a visitor
+  I want to see Mass Health information about my state
+  So that I can know more about Massachusetts
+} do
+
+  scenario 'user clicks on a query statement' do
+    visit root_path
+    click_on 'Cities, by population'
+    expect(page).to have_content("Population")
+    expect(page).to have_content("Boston")
+  end
+
+end
